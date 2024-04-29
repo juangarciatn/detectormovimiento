bdaaa3348e48938acdc422dd5aa2c32e4ddb28cc
juangarciatn <91282527+juangarciatn@users.noreply.github.com>
2024-04-29 12:14:14 +0200

    V1.1

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 wifi_config = {
-    'ssid':'MiFibra-69F5H',
-    'password':'Vr7boyYk'
+    'ssid':'SSID',
+    'password':'PASS'
 }
 
 utelegram_config = {
-    'token': '6732588344:AAFv7UPh3SzNqzrj0NJ_PIWcEpu1-OnITjg'
+    'token': 'TOKEN'
 }
